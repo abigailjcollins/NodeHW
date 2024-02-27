894f6d2393c5cf365f3535ac85e799fa4753cb92
Abigail Collins <abigailjcollins@gmail.com>
2024-02-26 19:36:08 -0500

    Node Day 1 HW

---
+++ modules.js
@@ -0,0 +1,10 @@
+// const xyz = require('./people');
+const { people, ages } = require('./people');
+
+console.log(people, ages);
+// console.log(xyz.people, xyz.ages);
+// console.log(people);
+
+const os = require('os');
+// console.log(os);
+console.log(os.platform(), os.homedir());
---
+++ people.js
@@ -0,0 +1,11 @@
+const people = ["Yoshi", "Ryu", "Chun-Li", "Mario"];
+const ages = [20, 25, 30, 35
+]
+console.log(people)
+
+// module.exports = "hello"
+
+module.exports = {
+    people,
+    ages
+}
